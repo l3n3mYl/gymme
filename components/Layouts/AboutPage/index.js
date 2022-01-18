b83432b0ab78a3b56084d815aa02102b2c7229d4
l3n3mYl <asavkinas123@gmail.com>
2022-01-18 16:59:43 +0000

    Added title and description components

---
+++ components/Layouts/AboutPage/index.js
@@ -0,0 +1,30 @@
+import styles from './About.module.scss'
+import React from 'react'
+import AnyImage from '../../Handlers/ImageHandler'
+import IconHeading from '../../IconHeading'
+import classNames from 'classnames'
+import BlockContent from '../../Handlers/BlockContentHandler'
+import { string, object, array } from 'prop-types'
+
+const AboutPage = ({ className, title, description, refer, id }) => {
+  return (
+    <div ref={refer} id={id} className={classNames(styles.AboutPage, className)}>
+      <IconHeading
+        icon='./Icons/PPL.png'
+        text={title}
+        className={styles.sectionName}
+      />
+      {description && <BlockContent className={styles.description} blocks={description} />}
+    </div>
+  )
+}
+
+AboutPage.propTypes = {
+  refer: object.isRequired,
+  id: string.isRequired,
+  title: string.isRequired,
+  description: array,
+  className: string,
+}
+
+export default AboutPage
