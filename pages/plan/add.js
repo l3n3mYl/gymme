f630127566da896a184e312cd298345794cbe458
l3n3mYl <asavkinas123@gmail.com>
2022-02-11 15:15:52 +0000

    Added css for User info page

--- pages/plan/add.js
+++ pages/plan/add.js
@@ -2,6 +2,7 @@ import Router from 'next/router'
 import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import { FetchJSON } from '../../functions/fetch'
+import Image from '../../components/Handlers/ImageHandler'
 import Container from '../../components/Handlers/ContentHandlers/Container'
 
 import styles from './styles/Add.module.scss'
@@ -78,9 +79,9 @@ const Add = () => {
       ).then((rez) => {
         if (rez.status === 401)
           setRegisterErrors('Something went wrong when validating your details')
-        // else {
-        //   Router.push('/')
-        // }
+        else {
+          Router.push('/')
+        }
       })
     }
   }
@@ -91,145 +92,287 @@ const Add = () => {
   }
 
   return (
-    <Container size="small" gutter center>
+    <Container className={styles.Container} size="small" gutter center>
+      <h1>
+        Subscribe to
+        <br />
+        {router.query.plan}
+      </h1>
       <form onSubmit={(e) => validateForm(e, formValues)}>
-        <input
-          onChange={handleChange}
-          value={formValues.cardNumber}
-          type="text"
-          name="cardNumber"
-          id="cardNumber"
-          placeholder="Card Number"
-        />
-        <label htmlFor="cardNumber">Card Number</label>
-        <p className={styles.error}>
-          {formErrors.cardNumber && formErrors.cardNumber}
-        </p>
+        <div className={styles.Field}>
+          <label htmlFor="cardNumber">Card Number</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/credit-card.svg"
+              width={26}
+              height={26}
+              alt="Credit Card Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.cardNumber}
+              type="text"
+              name="cardNumber"
+              id="cardNumber"
+              placeholder="Card Number"
+            />
+          </div>
+          <p className={styles.error}>
+            {formErrors.cardNumber && formErrors.cardNumber}
+          </p>
+        </div>
 
-        <input
-          onChange={handleChange}
-          value={formValues.expDate}
-          type="date"
-          name="expDate"
-          id="expDate"
-          placeholder="Expiry Date"
-        />
-        <label htmlFor="expDate">Expiry date</label>
+        <div className={styles.Field}>
+          <label htmlFor="expDate">Expiry date</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/calendar.svg"
+              width={26}
+              height={26}
+              alt="Calendar Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.expDate}
+              type="date"
+              name="expDate"
+              id="expDate"
+              placeholder="Expiry Date"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.cvv}
-          type="text"
-          name="cvv"
-          id="cvv"
-          placeholder="CVV"
-        />
-        <label htmlFor="cvv">CVV</label>
+        <div className={styles.Field}>
+          <label htmlFor="cvv">CVV</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/cvv.svg"
+              width={26}
+              height={26}
+              alt="CVV Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.cvv}
+              type="text"
+              name="cvv"
+              id="cvv"
+              placeholder="CVV"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.name}
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Name on Card"
-        />
-        <label htmlFor="name">Name on Card</label>
+        <div className={styles.Field}>
+          <label htmlFor="name">Name on Card</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/login.svg"
+              width={26}
+              height={26}
+              alt="Name Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.name}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Name on Card"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.country}
-          type="text"
-          name="country"
-          id="country"
-          placeholder="Country"
-        />
-        <label htmlFor="country">Country</label>
+        <div className={styles.Field}>
+          <label htmlFor="country">Country</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/countries.svg"
+              width={26}
+              height={26}
+              alt="Country Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.country}
+              type="text"
+              name="country"
+              id="country"
+              placeholder="Country"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.state}
-          type="text"
-          name="state"
-          id="state"
-          placeholder="State"
-        />
-        <label htmlFor="state">State</label>
+        <div className={styles.Field}>
+          <label htmlFor="state">State</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/state.svg"
+              width={26}
+              height={26}
+              alt="State Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.state}
+              type="text"
+              name="state"
+              id="state"
+              placeholder="State"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.address1}
-          type="text"
-          name="address1"
-          id="address1"
-          placeholder="Address 1"
-        />
-        <label htmlFor="address1">Address 1</label>
+        <div className={styles.Field}>
+          <label htmlFor="address1">Address 1</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/pin.svg"
+              width={26}
+              height={26}
+              alt="Address Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.address1}
+              type="text"
+              name="address1"
+              id="address1"
+              placeholder="Address 1"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.address2}
-          type="text"
-          name="address2"
-          id="address2"
-          placeholder="Address 2"
-        />
-        <label htmlFor="address2">Address 2</label>
+        <div className={styles.Field}>
+          <label htmlFor="address2">Address 2</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/address.svg"
+              width={26}
+              height={26}
+              alt="Address Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.address2}
+              type="text"
+              name="address2"
+              id="address2"
+              placeholder="Address 2"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.city}
-          type="text"
-          name="city"
-          id="city"
-          placeholder="City"
-        />
-        <label htmlFor="city">City</label>
+        <div className={styles.Field}>
+          <label htmlFor="city">City</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/city.svg"
+              width={26}
+              height={26}
+              alt="City Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.city}
+              type="text"
+              name="city"
+              id="city"
+              placeholder="City"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.postCode}
-          type="text"
-          name="postCode"
-          id="postCode"
-          placeholder="Post Code"
-        />
-        <label htmlFor="postCode">Post Code</label>
+        <div className={styles.Field}>
+          <label htmlFor="postCode">Post Code</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/zip-code.svg"
+              width={26}
+              height={26}
+              alt="Post Code Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.postCode}
+              type="text"
+              name="postCode"
+              id="postCode"
+              placeholder="Post Code"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.phone}
-          type="text"
-          name="phone"
-          id="phone"
-          placeholder="Phone Number"
-        />
-        <label htmlFor="phone">Phone Number</label>
+        <div className={styles.Field}>
+          <label htmlFor="phone">Phone Number</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/phone.svg"
+              width={26}
+              height={26}
+              alt="Phone Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.phone}
+              type="text"
+              name="phone"
+              id="phone"
+              placeholder="Phone Number"
+            />
+          </div>
+        </div>
         <br />
 
-        <input
-          onChange={handleChange}
-          value={formValues.email}
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-        />
-        <label htmlFor="email">Email</label>
-        <p className={styles.error}>{formErrors.email && formErrors.email}</p>
+        <div className={styles.Field}>
+          <label htmlFor="email">Email</label>
+          <div className={styles.InputWithIcon}>
+            <Image
+              className={styles.Image}
+              image="/FormIcons/email.svg"
+              width={26}
+              height={26}
+              alt="Email Icon"
+            />
+            <input
+              onChange={handleChange}
+              value={formValues.email}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+            />
+          </div>
+          <p className={styles.error}>{formErrors.email && formErrors.email}</p>
+        </div>
 
         <p className={styles.error}>{registerErrors && registerErrors}</p>
 
-        <input type="submit" name="submit" id="submit" placeholder="Submit" />
+        <input
+          type="submit"
+          name="submit"
+          id={styles.Submit}
+          placeholder="Submit"
+        />
       </form>
     </Container>
   )
