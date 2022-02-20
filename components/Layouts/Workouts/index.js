b5f1cf10ed69f6b0f26c7cf900cf72eb5a55e74a
l3n3mYl <asavkinas123@gmail.com>
2022-02-20 09:05:53 +0000

    Fixed an issue with only half of swiper elements loading

--- components/Layouts/Workouts/index.js
+++ components/Layouts/Workouts/index.js
@@ -4,7 +4,7 @@ import styles from './Workouts.module.scss'
 import IconHeading from '../../IconHeading'
 import AnyImage from '../../Handlers/ImageHandler'
 import 'swiper/css'
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef } from 'react'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Navigation } from 'swiper'
@@ -17,11 +17,6 @@ const Workouts = ({ title, offerings, id, className }) => {
 
   const prevRef = useRef(null)
   const nextRef = useRef(null)
-  const [swiperLoading, setSwiperLoading] = useState(true)
-
-  useEffect(() => {
-    setSwiperLoading(false)
-  }, [])
 
   return (
     <Container center>
@@ -71,7 +66,6 @@ const Workouts = ({ title, offerings, id, className }) => {
             />
           </div>
           {offerings &&
-            !swiperLoading &&
             offerings.map((offer) => {
               return (
                 <SwiperSlide key={offer._key} className={styles.Card}>
